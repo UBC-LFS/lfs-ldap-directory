b8138a1f722e6109372388a8a327493b993b20d8
Justin Lee <justinlee0022@gmail.com>
2018-04-19 14:36:07 -0700

    Added cors package

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const ldap = require('ldapjs')
+const cors = require('cors')
 
 const app = express()
 
+const corsOptions = {
+  origin: 'http://lfs-ps.sites.olt.ubc.ca'
+}
+
 const client = ldap.createClient({
-  url: 'ldap://ldap.ubc.ca'
+  url: 'ldap://ldap.ubc.ca',
+  optionsSuccessStatus: 200
 })
 
 const opts = {
@@ -27,13 +33,7 @@ const search = new Promise((resolve, reject) => {
   })
 })
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://lfs-ps.sites.olt.ubc.ca')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-
-app.get('/lfsdir', (req, res) => {
+app.get('/lfsdir', cors(corsOptions), (req, res) => {
   search.then(result => res.send(result))
 })
 
