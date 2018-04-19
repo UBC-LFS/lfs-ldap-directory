063afd91af8e9ae3982d75131502fa7c1cb50db5
Justin Lee <justinlee0022@gmail.com>
2018-04-19 11:06:08 -0700

    Modified script to run with jquery

--- public/index.js
+++ public/index.js
@@ -1,39 +1,38 @@
-/* global XMLHttpRequest, $ */
+/* global XMLHttpRequest, jQuery */
 
-const generateTable = (staffList) => {
-  const cleanStaffList = staffList.map(staff => ({
-    firstName: staff.givenName,
-    lastName: staff.sn,
-    phone: Array.isArray(staff.telephoneNumber) ? staff.telephoneNumber[0] : staff.telephoneNumber,
-    email: staff.mail,
-    office: Array.isArray(staff.l) ? staff.l[0] : staff.l,
-    title: staff.title
-  }))
-  const dataForTable = cleanStaffList.map(staff => (
-    [
-      staff.lastName,
-      staff.firstName,
-      staff.title,
-      staff.email,
-      staff.office,
-      staff.phone
-    ]
-  ))
-  $('#staffList').DataTable({
-    data: dataForTable,
-    columns: [
-      { title: 'Last Name', 'defaultContent': '' },
-      { title: 'First Name', 'defaultContent': '' },
-      { title: 'Title', 'defaultContent': '' },
-      { title: 'Email', 'defaultContent': '' },
-      { title: 'Office', 'defaultContent': '' },
-      { title: 'Phone', 'defaultContent': '' }
-    ],
-    'pageLength': 100
-  })
-}
-
-document.addEventListener('DOMContentLoaded', function ($) {
+jQuery(document).ready(function ($) {
+  const generateTable = (staffList) => {
+    const cleanStaffList = staffList.map(staff => ({
+      firstName: staff.givenName,
+      lastName: staff.sn,
+      phone: Array.isArray(staff.telephoneNumber) ? staff.telephoneNumber[0] : staff.telephoneNumber,
+      email: staff.mail,
+      office: Array.isArray(staff.l) ? staff.l[0] : staff.l,
+      title: staff.title
+    }))
+    const dataForTable = cleanStaffList.map(staff => (
+      [
+        staff.lastName,
+        staff.firstName,
+        staff.title,
+        staff.email,
+        staff.office,
+        staff.phone
+      ]
+    ))
+    $('#staffList').DataTable({
+      data: dataForTable,
+      columns: [
+        { title: 'Last Name', 'defaultContent': '' },
+        { title: 'First Name', 'defaultContent': '' },
+        { title: 'Title', 'defaultContent': '' },
+        { title: 'Email', 'defaultContent': '' },
+        { title: 'Office', 'defaultContent': '' },
+        { title: 'Phone', 'defaultContent': '' }
+      ],
+      'pageLength': 100
+    })
+  }
   const xhr = new XMLHttpRequest()
 
   xhr.onreadystatechange = function () {
